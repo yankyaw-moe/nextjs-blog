524e0eb288e0fc6cd6629e40ffbc7cf6488ef97c
Yan Kyaw Moe <yankyawmoe.dev@gmail.com>
2025-05-06 14:17:01 +0630

    updated data

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,12 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>
+          I have over 5 years of experience in software development,
+          maintenance, database design, project requirements analysis, team
+          leadership, and software deployment on various servers. I mainly use
+          JavaScript.
+        </p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
